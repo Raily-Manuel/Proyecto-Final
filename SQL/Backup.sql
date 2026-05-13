b3c064849520840698b055acf207f12ba1abd634
The Algorithm Kaiser <angelmercedesventura72@gmail.com>
2026-05-13 15:32:40 +0200

    Add comments and structure to backup SQL script

--- SQL/Backup.sql
+++ SQL/Backup.sql
@@ -1,5 +1,4 @@
---Backup Completo
-
+-- Backup Completo
 BACKUP DATABASE [GasolineraDB]
 TO DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Backup\GasolineraDB_FULL.bak'
 WITH 
@@ -9,7 +8,8 @@ NAME = N'GasolineraDB Full Backup',
 STATS = 10;
 GO
 
---Backup Diferencial
+
+-- Backup Diferencial
 BACKUP DATABASE [GasolineraDB]
 TO DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Backup\GasolineraDB_DIFF.bak'
 WITH 
@@ -19,11 +19,14 @@ NAME = N'GasolineraDB Differential Backup',
 STATS = 10;
 GO
 
---Backup de Registro de Transacciones
+
+-- Cambiar A Recovery Full
 ALTER DATABASE [GasolineraDB]
 SET RECOVERY FULL;
 GO
 
+
+-- Backup Del Log
 BACKUP LOG [GasolineraDB]
 TO DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Backup\GasolineraDB_LOG.trn'
 WITH 
@@ -32,17 +35,59 @@ NAME = N'GasolineraDB Log Backup',
 STATS = 10;
 GO
 
---Verifica existencia del completo
+
+-- Verificar Backup Completo
 RESTORE HEADERONLY
 FROM DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Backup\GasolineraDB_FULL.bak';
 GO
 
---Verifica existencia del diferencial
+
+-- Verificar Backup Diferencial
 RESTORE HEADERONLY
 FROM DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Backup\GasolineraDB_DIFF.bak';
 GO
 
---Verifica existencia del log
+
+-- Verificar Backup Log
 RESTORE HEADERONLY
 FROM DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Backup\GasolineraDB_LOG.trn';
+GO
+
+
+-- Cerrar conexiones
+ALTER DATABASE [GasolineraDB]
+SET SINGLE_USER
+WITH ROLLBACK IMMEDIATE;
+GO
+
+-- Restaurar Backup Completo
+RESTORE DATABASE [GasolineraDB]
+FROM DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Backup\GasolineraDB_FULL.bak'
+WITH 
+REPLACE,
+NORECOVERY;
+GO
+
+
+-- Restaurar Backup Diferencial
+RESTORE DATABASE [GasolineraDB]
+FROM DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Backup\GasolineraDB_DIFF.bak'
+WITH NORECOVERY;
+GO
+
+
+-- Restaurar Backup Log
+RESTORE LOG [GasolineraDB]
+FROM DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL17.MSSQLSERVER\MSSQL\Backup\GasolineraDB_LOG.trn'
+WITH RECOVERY;
+GO
+
+
+-- Volver Multiusuario
+ALTER DATABASE [GasolineraDB]
+SET MULTI_USER;
+GO
+
+RESTORE DATABASE [GasolineraDB]
+WITH RECOVERY;
 GO
